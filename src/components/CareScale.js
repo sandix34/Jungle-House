581f8180bc6b3f4c74d42848b0aef2c09e9ce72f
sandix34 <sandrinezocca@gmail.com>
2022-05-24 17:52:00 +0200

    add sun and water svg

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,8 +1,11 @@
+import  Sun from '../assets/sun.svg';
+import  Water from '../assets/water.svg';
+
 function CareScale({scaleValue, careType}) {
     //const {scaleValue, careType} = props
 
     const range = [1, 2, 3]
-    const scaleType = careType === 'light' ? '☀️' : '💧';
+    const scaleType = careType === 'light' ? (<img src={Sun} alt='sun-icon' />) : (<img src={Water} alt='water-icon' />);
 
     return (
         <div>
